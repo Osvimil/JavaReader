d734394f268b87188d458ba4562af8d644c4a9e9
oswaldo <oswaldoadidas@hotmail.com>
2017-01-17 20:24:39 -0600

    adding files about reading files into a java file

---
+++ src/archivos/Escritura.java
@@ -0,0 +1,34 @@
+
+package archivos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+
+
+public class Escritura {
+    
+    public static void main(String[] args) throws IOException {
+        
+        FileWriter fw = new FileWriter("datos.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        
+        Scanner sc = new Scanner(System.in);
+        String cadena1;
+        String seguir="";
+        
+        do{
+            System.out.println("Write something");
+            cadena1 = sc.nextLine();
+            bw.write(cadena1);
+            bw.newLine();
+            System.out.println("Continuar? Y/N");
+            seguir= sc.nextLine();
+        
+        }while(!seguir.equalsIgnoreCase("no"));
+        bw.close();
+        fw.close();
+    }
+    
+}
